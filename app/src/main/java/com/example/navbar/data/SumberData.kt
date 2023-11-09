0d1148ea9502ab0d3d5f69c7906884a89a49afb2
Fahri Hidayat <m.fahri.ft21@mail.umy.ac.id>
2023-11-09 08:23:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navbar/data/SumberData.kt
@@ -0,0 +1,11 @@
+package com.example.navbar.data
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}
