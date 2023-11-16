60544360e737495ee10c655d2166802b9b0de763
Fahri Hidayat <m.fahri.ft21@mail.umy.ac.id>
2023-11-16 17:52:17 +0700

    - Menambahkan var nama,no telepon,alamat - Membuat var listData untuk menggabungkan nama,no telepon,alamat menjadi variabel String

--- app/src/main/java/com/example/navbar/ui/theme/DataPelanggan.kt
+++ app/src/main/java/com/example/navbar/ui/theme/DataPelanggan.kt
@@ -1,11 +1,19 @@
 package com.example.navbar.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun DataPelanggan(
     onSubmitButtonClicked : (MutableList<String>) -> Unit,
     onCancelButtonClicked : () -> Unit
 ) {
+    var nama by remember { mutableStateOf(value = "") }
+    var noTelepon by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var listData: MutableList<String> = mutableListOf(nama,noTelepon,alamat)
 
 }
