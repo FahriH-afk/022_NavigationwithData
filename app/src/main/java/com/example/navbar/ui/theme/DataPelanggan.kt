17e4f9332b1954440c0273824646d80fde6a41d7
Fahri Hidayat <m.fahri.ft21@mail.umy.ac.id>
2023-11-16 18:33:44 +0700

    - Menambahkan OutlineButton cancel

--- app/src/main/java/com/example/navbar/ui/theme/DataPelanggan.kt
+++ app/src/main/java/com/example/navbar/ui/theme/DataPelanggan.kt
@@ -2,11 +2,13 @@ package com.example.navbar.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,9 +18,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.navbar.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,6 +66,19 @@ fun DataPelanggan(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(15.dp))
+        Spacer(modifier = Modifier.padding(2.dp))
+
+        Row (modifier = Modifier
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .weight(1f, false),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom) {
+
+            OutlinedButton(
+                onClick = onCancelButtonClicked) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+        }
     }
 
 }
