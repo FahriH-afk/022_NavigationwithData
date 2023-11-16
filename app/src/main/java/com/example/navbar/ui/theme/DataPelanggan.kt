c5a6d1cdab168538412634c15df2526d2f6fbf26
Fahri Hidayat <m.fahri.ft21@mail.umy.ac.id>
2023-11-16 17:38:17 +0700

    - Membuat function Datapelanggan

--- app/src/main/java/com/example/navbar/ui/theme/DataPelanggan.kt
+++ app/src/main/java/com/example/navbar/ui/theme/DataPelanggan.kt
@@ -1,2 +1,11 @@
 package com.example.navbar.ui.theme
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DataPelanggan(
+    onSubmitButtonClicked : (MutableList<String>) -> Unit,
+    onCancelButtonClicked : () -> Unit
+) {
+
+}
